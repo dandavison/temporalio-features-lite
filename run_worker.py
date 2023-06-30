9dbf355b0827e0e9e22924c2d8d7d161953ef86a
Dan Davison <dandavison7@gmail.com>
2023-06-30 09:07:37 -0400

    logging

--- run_worker.py
+++ run_worker.py
@@ -1,18 +1,24 @@
 import asyncio
+import logging
 
-from temporalio import activity, workflow
 from temporalio.client import Client
 from temporalio.worker import Worker
 
 import activities
 import workflows
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 async def main():
+    logger.info("creating client...")
     client = await Client.connect("localhost:7233", namespace="default")
+    logger.info("created client")
     worker = Worker(
         client, task_queue="hello-task-queue", workflows=[workflows.SayHello], activities=[activities.say_hello]
     )
+    logger.info("created worker")
     await worker.run()
 
 
