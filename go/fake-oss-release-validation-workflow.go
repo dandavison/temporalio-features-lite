c8e63147f445a6fc28e004d36ba18fe325f9a8e4
Dan Davison <dan.davison@temporal.io>
2023-08-09 12:42:58 -0400

    WIP: results schema

--- go/fake-oss-release-validation-workflow.go
+++ go/fake-oss-release-validation-workflow.go
@@ -9,9 +9,40 @@ import (
 
 type (
 	Status string
+
+	RunTestsInput struct {
+		TemporalAddress   string
+		BenchTestDuration string
+	}
+
+	StringDatum struct {
+		Name  string
+		Value string
+	}
+
+	Description []StringDatum
+
+	Link struct {
+		Text string
+		Url  string
+	}
+
+	RunBenchTestsActivityOutput struct {
+		Links []Link
+	}
+
+	TestOutput struct {
+		Description Description
+		Output      RunBenchTestsActivityOutput
+	}
+
+	RunTestsOutput struct {
+		TestsOutput []TestOutput
+	}
+
 	Output struct {
-		LogUrls []string
-		Success bool
+		Description []StringDatum
+		Output      RunTestsOutput
 	}
 )
 
@@ -26,21 +57,38 @@ func FakeOSSReleaseValidationWorkflow(ctx workflow.Context) (Output, error) {
 	fmt.Println("Wf: Starting")
 	var status = InProgress
 
+	output := Output{
+		Description: []StringDatum{{Name: "Run", Value: "Fake OSS Server validation run 1"}, {Name: "Server version", Value: "fake-server-release-tag"}},
+	}
 	err := workflow.SetQueryHandler(ctx, "getStatus", func() (Status, error) { return status, nil })
 	if err != nil {
-		return Output{
-			LogUrls: []string{},
-			Success: false,
-		}, err
+		return output, err
 	}
 
 	fmt.Println("Wf: Sleeping")
 	workflow.Sleep(ctx, 30*time.Second)
 	fmt.Println("Wf: Done")
 
+	output.Output = RunTestsOutput{
+		TestsOutput: []TestOutput{
+			{
+				Description: []StringDatum{{Name: "Test name", Value: "Test 1"}},
+				Output: RunBenchTestsActivityOutput{
+					Links: []Link{
+						{
+							Text: "Log link 1",
+							Url:  "https://www.popsci.com/uploads/2023/06/06/lumber.png?auto=webp&width=1440&height=810",
+						},
+						{
+							Text: "Log link 2",
+							Url:  "https://www.perc.org/wp-content/uploads/2023/04/logging-1.jpg",
+						},
+					},
+				},
+			},
+		},
+	}
+
 	status = Done
-	return Output{
-		LogUrls: []string{"https://www.popsci.com/uploads/2023/06/06/lumber.png?auto=webp&width=1440&height=810"},
-		Success: true,
-	}, nil
+	return output, nil
 }
